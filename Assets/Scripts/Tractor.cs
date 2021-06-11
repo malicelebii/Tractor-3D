3969924c2373b820f92fdc7c020bd50f575d650a
malicelebi_ <mcelebi@verisis.com.tr>
2021-06-12 01:23:24 +0300

    First adjustments

--- Assets/Scripts/Tractor.cs
+++ Assets/Scripts/Tractor.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Tractor : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+    [SerializeField] float hmoveSpeed;
+
+    private bool isSucceed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(0, 0, Time.deltaTime * moveSpeed);
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * hmoveSpeed, 0, 0);
+    }
+
+
+
+
+
+    void OnTriggerEnter()
+    {
+
+    }
+    void OnTriggerExit()
+    {
+
+    }
+
 }
