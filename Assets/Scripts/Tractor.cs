e1723a61195d9cad182b7a289f404af42717910f
malicelebi_ <mcelebi@verisis.com.tr>
2021-06-12 18:30:55 +0300

    cinemachine

--- Assets/Scripts/Tractor.cs
+++ Assets/Scripts/Tractor.cs
@@ -6,14 +6,19 @@ public class Tractor : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] float hmoveSpeed;
+    [SerializeField] GameObject extraTrailer;
     [SerializeField] GameObject[] trailers;
 
+    public Animator tractor;
+
+
+
     private bool isSucceed;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-
+        tractor.SetBool("isMoving", true);
     }
 
     // Update is called once per frame
@@ -26,7 +31,7 @@ public class Tractor : MonoBehaviour
     {
         transform.position += new Vector3(0, 0, Time.deltaTime * moveSpeed);
         transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * hmoveSpeed, 0, 0);
-        transform.forward = new Vector3(0,0,1);
+        transform.forward = new Vector3(1, 0, 0);
     }
 
 
@@ -39,6 +44,8 @@ public class Tractor : MonoBehaviour
         {
             Debug.Log("ExtraTrailer");
             Destroy(collider.gameObject);
+            GameObject newTrailer = Instantiate(extraTrailer, trailers[trailers.Length - 1].transform.position - new Vector3(0, 0, 4.5f), Quaternion.identity);
+
             foreach (var trailer in trailers)
             {
                 // trailer.transform.localScale += new Vector3(0, transform.localScale.y, 0);
